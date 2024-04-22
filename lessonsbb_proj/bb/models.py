2c58ddfc26562e5fe15fa2808978dd23e54c6367
Artem <mrkozlovartem@gmail.com>
2024-04-22 08:39:39 +0300

    lesson 4

--- lessonsbb_proj/bb/models.py
+++ lessonsbb_proj/bb/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Rubric(models.Model):
+    """Модель рубрики"""
+    name = models.CharField(max_length=100,
+                            verbose_name="Название",
+                            unique=True)
+    parent = models.ForeignKey('self',
+                               on_delete=models.SET_NULL,
+                               verbose_name="Родительская рубрика",
+                               null=True)
+
+
+class Bulletin(models.Model):
+    """Модель объявления"""
+    name = models.CharField(max_length=100,
+                            verbose_name="Название")
+    description = models.TextField(verbose_name="Описание",
+                                   help_text="Текст неограниченной длинны")
+    cost = models.DecimalField(decimal_places=2, max_digits=11)
+    curr = models.CharField(max_length=3,
+                            verbose_name="Валюта")
+    rubric = models.ForeignKey(Rubric,
+                               on_delete=models.PROTECT,
+                               verbose_name="Рубрика")
