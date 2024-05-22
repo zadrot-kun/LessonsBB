2b73f30a426383d974a106cecedcd9b87aeea246
Artem <mrkozlovartem@gmail.com>
2024-05-22 09:55:07 +0300

    lesson 10

--- lessonsbb_proj/bb/admin.py
+++ lessonsbb_proj/bb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bb.models import Rubric
 
-# Register your models here.
+
+class ParentEditor(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    list_display_links = ('name', )
+
+
+admin.site.register(Rubric, ParentEditor)
--- lessonsbb_proj/bb/apps.py
+++ lessonsbb_proj/bb/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BbConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bb'
+    verbose_name = 'Модель объявлений и рубрик'
