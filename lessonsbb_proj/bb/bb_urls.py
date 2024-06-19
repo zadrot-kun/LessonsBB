3df8cb3b8a19c2eb5369937fbe0f3a86807bc605
Artem <mrkozlovartem@gmail.com>
2024-06-19 17:11:33 +0300

    lesson 18

--- lessonsbb_proj/bb/bb_urls.py
+++ lessonsbb_proj/bb/bb_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from bb.views import CreateRubricController, rubric_list, CreateBBController, index_by_rubric
+from bb.views import CreateRubricController, rubric_list, CreateBBController, index_by_rubric, create_bb
 
 urlpatterns = [
     path('create_rubric/', CreateRubricController.as_view(), name="create_rubric"),
-    path('create_bb/', CreateBBController.as_view(), name="create_bb",),
+    # path('create_bb/', CreateBBController.as_view(), name="create_bb",),
+    path('create_bb/', create_bb, name="create_bb",),
     path('json_rubrics/', rubric_list),
     path('rubrics/<int:rubric>/', index_by_rubric, name="bb_by_rubric")
 ]
--- lessonsbb_proj/bb/views.py
+++ lessonsbb_proj/bb/views.py
@@ -5,6 +5,7 @@ from bb.forms import BBForm, RubricForm
 from django.http import JsonResponse
 from django.template.response import TemplateResponse
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 class CreateRubricController(CreateView):
@@ -14,6 +15,33 @@ class CreateRubricController(CreateView):
 
     def get_success_url(self):
         return reverse('index')
+
+
+def create_bb(request):
+    template_name = "bb/new_record.html"
+    if request.method == 'GET':
+        bb_form = BBForm()
+        return TemplateResponse(request,
+                                template_name,
+                                context={'form': bb_form})
+    elif request.method == 'POST':
+        bb_form = BBForm(request.POST, request.FILES)
+        if bb_form.is_valid():
+            # bb_form.save()
+            new_bb = BulletinModel(name=bb_form.cleaned_data['name'],
+                                   description=bb_form.cleaned_data['description'],
+                                   cost=bb_form.cleaned_data['cost'],
+                                   curr=bb_form.cleaned_data['curr'],
+                                   rubric=bb_form.cleaned_data['rubric'],
+                                   picture=bb_form.cleaned_data['picture'])
+            new_bb.save()
+            new_bb.description += '!'
+            new_bb.save()
+            return HttpResponseRedirect("/")
+        else:
+            return TemplateResponse(request,
+                                    template_name,
+                                    context={'form': bb_form})
 
 
 class CreateBBController(CreateView):
