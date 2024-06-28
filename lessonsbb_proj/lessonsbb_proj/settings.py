8c15fd844f5c70ab3d45e47cd2d012fd4bda3ea6
Artem <mrkozlovartem@gmail.com>
2024-06-28 09:57:32 +0300

    lesson 20

--- lessonsbb_proj/lessonsbb_proj/settings.py
+++ lessonsbb_proj/lessonsbb_proj/settings.py
@@ -76,13 +76,25 @@ WSGI_APPLICATION = 'lessonsbb_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Test DB
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Prod DB
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': '1234554321',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
